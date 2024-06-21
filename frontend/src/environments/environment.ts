34d235612d6248e0fbea296238237aada288b31a
naichinger <aichinger.niklas@gmail.com>
2024-06-21 21:58:45 +0200

    add frontend docker

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    API_URL: "http://localhost:8080/api",
-    BACKEND_BASE_HREF: "http://localhost:8080"
+    API_URL: "https://shrt.naichinger.com/api",
+    BACKEND_BASE_HREF: "https://shrt.naichinger.com"
 };
